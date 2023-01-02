c8668906b5170f466acffc2f766d036d430883cb
Manu Sharma <manu231295@gmail.com>
2023-01-02 16:01:32 +0530

    Add files via upload

---
+++ Killing Task.ps1
@@ -0,0 +1,23 @@
+﻿$process = Get-Process msedgewebview2
+$web = "msedgewebview2"
+
+
+
+foreach ($web in $process){
+    taskkill /F /IM msedgewebview2.exe
+}
+
+
+taskkill /F /IM Spotify.exe
+
+taskkill /F /IM MSEdge.exe
+
+taskkill /F /IM PSAgent.exe
+
+taskkill /F /IM igfxEM.exe
+
+taskkill /F /IM PhoneExperienceHost.exe
+
+taskkill /F /IM PSSvc.exe
+
+taskkill /F /IM PredatorSense.exe
