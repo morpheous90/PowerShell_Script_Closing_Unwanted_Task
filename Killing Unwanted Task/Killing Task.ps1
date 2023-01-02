c176e8e921b38bdee71c15a86b73e0f5367cd61e
Manu Sharma <manu231295@gmail.com>
2023-01-02 15:30:12 +0530

    initial commit

---
+++ Killing Unwanted Task/Killing Task.ps1
@@ -0,0 +1,28 @@
+﻿#$task = gps | Where-Object {$_.Name -match "msedgewebview2.exe"}
+
+# $service  =  Get-Process | Where-Object {$_.Name = "msedgewebview2.exe"}
+# $service
+$process = Get-Process msedgewebview2
+$web = "msedgewebview2"
+# $process
+
+#for each should work here.
+foreach ($web in $process){
+    taskkill /F /IM msedgewebview2.exe
+    echo "Web"
+}
+
+
+taskkill /F /IM Spotify.exe
+
+taskkill /F /IM MSEdge.exe
+
+taskkill /F /IM PSAgent.exe
+
+taskkill /F /IM igfxEM.exe
+
+taskkill /F /IM PhoneExperienceHost.exe
+
+taskkill /F /IM PSSvc.exe
+
+taskkill /F /IM PredatorSense.exe
